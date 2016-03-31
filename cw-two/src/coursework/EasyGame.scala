c5b43769d7659df8e6ea32207f3a292bbe4375a4
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 14:00:06 +0100

    Update EasyGame.scala
    Added Scaladoc

--- cw-two/src/coursework/EasyGame.scala
+++ cw-two/src/coursework/EasyGame.scala
@@ -2,6 +2,14 @@ package coursework
 
 import com.google.inject.Inject;
 
+/**
+ * A version of Mastermind which displays the Secret Code.
+ * Inherits the functionality of HardGame.
+ * 
+ * @param guess the service to request guesses from a user. Injected by Google Guice, @see GameModule.
+ * @param feedback the service to produce Mastermind feedback. Injected by Google Guice, @see GameModule.
+ * @param secretCode the service to produce a secret Mastermind code. Injected by Google Guice, @see GameModule.
+ */
 class EasyGame @Inject() (guess: Guess, feedback: Feedback, secretCode: SecretCode) extends HardGame(guess, feedback, secretCode) {
   override val showCode = true
 }
