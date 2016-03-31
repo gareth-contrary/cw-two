0f23267ce0064f3d6dd79ce0d8ea65dd2f656448
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:23:56 +0100

    Update Feedback.scala
    added Scaladoc

--- cw-two/src/coursework/Feedback.scala
+++ cw-two/src/coursework/Feedback.scala
@@ -1,5 +1,16 @@
 package coursework
 
+/**
+ * Provides a service for providing Mastermind feedback.
+ */
 trait Feedback {
+  
+  /**
+   * Provides Mastermind Feedback.
+   * 
+   * @param code the game's Secret Code which is represented as a sequence of Pegs.
+   * @param guess the user's guess represented as a sequence of Pegs.
+   * @return feedback represented as a sequence of FeedbackPegs.
+   */
   def getFeedback(code: Seq[Peg], guess: Seq[Peg]): Seq[FeedbackPeg]
 }
