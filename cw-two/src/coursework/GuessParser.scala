14422230526abd30f89a1d35a87e15fc1a941597
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:38:54 +0100

    Update GuessParser.scala
    Added Scaladoc

--- cw-two/src/coursework/GuessParser.scala
+++ cw-two/src/coursework/GuessParser.scala
@@ -1,6 +1,25 @@
 package coursework
 
+/**
+ * A parser for validating and transforming user input into a sequence of Pegs.
+ */
 trait GuessParser {
+  
+  /**
+   * Validates user input.
+   * 
+   * @param guess the user input.
+   * @param rowSize the number of Pegs being used in this Game.
+   * @return whether the guess is valid.
+   */
   def isValid(guess: String, rowSize: Int): Boolean
+  
+  /**
+   * Transforms user input into a sequence of Pegs. Implicity expects input 
+   * has been validated.
+   * 
+   * @param guess the user input.
+   * @return a sequence of Pegs.
+   */
   def parse(guess: String): Seq[Peg]
 }
