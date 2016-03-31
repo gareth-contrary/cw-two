ddc5501fd7cbd064683813dec1101bd8a912535a
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:50:23 +0100

    Update SecretCode.scala
    Added Scaladoc

--- cw-two/src/coursework/SecretCode.scala
+++ cw-two/src/coursework/SecretCode.scala
@@ -1,5 +1,14 @@
 package coursework
 
+/**
+ * A servie for generating Matermind's secret code.
+ */
 trait SecretCode {
+  
+  /**
+   * Generate a random sequence of Pegs.
+   * 
+   * @param rowSize the number od Pegs used in this game.
+   */
   def generateCode(rowSize: Int): Seq[Peg]
 }
