79a18f6f24595420e8b181c5f770ceffe818951d
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:18:44 +0100

    Update Guess.scala
    Add Scaladoc

--- cw-two/src/coursework/Guess.scala
+++ cw-two/src/coursework/Guess.scala
@@ -1,5 +1,12 @@
 package coursework
-
+/**
+ * Provides a service for requesting a guess from the user.
+ */
 trait Guess {
+  /**
+   * Requests a guess from the user.
+   * 
+   * @param rowSize the number of pegs in each guess.
+   */
   def requestGuess(rowSize: Int): Seq[Peg]
 }
