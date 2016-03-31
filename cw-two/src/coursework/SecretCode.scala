f51e055b28392da814ec9540307f12e2c29e77be
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:52:06 +0100

    Update SecretCode.scala
    added @return

--- cw-two/src/coursework/SecretCode.scala
+++ cw-two/src/coursework/SecretCode.scala
@@ -9,6 +9,7 @@ trait SecretCode {
    * Generate a random sequence of Pegs.
    * 
    * @param rowSize the number od Pegs used in this game.
+   * @return a secret code represented as a sequence of Pegs.
    */
   def generateCode(rowSize: Int): Seq[Peg]
 }
