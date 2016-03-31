c769676027b9f364205edb9c65c994dfdeb2df39
Gareth Moore <garethmoore26@gmail.com>
2016-03-31 13:51:16 +0100

    Update SecretCodeImpl.scala
    Added Scaladoc

--- cw-two/src/coursework/SecretCodeImpl.scala
+++ cw-two/src/coursework/SecretCodeImpl.scala
@@ -2,6 +2,9 @@ package coursework
 
 import scala.util.Random
 
+/**
+ * Implements the trait SecretCode.
+ */
 class SecretCodeImpl() extends SecretCode {
   def generateCode(rowSize: Int): Seq[Peg] = {
     val r = Random
